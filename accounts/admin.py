33d0c5ba482ac702a6d8fde07ef0d6cbdd341dfa
Vivek <vivekjangid608@gmail.com>
2019-05-29 12:26:05 +0530

    firstcommit

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,3 @@
 from django.contrib import admin
-from .models import *
 
 # Register your models here.
-admin.site.register(Candidate)
-admin.site.register(Project)
-admin.site.register(Assignment)
--- questionassigner/settings.py
+++ questionassigner/settings.py
@@ -20,7 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'api',
     'accounts',
-    'frontend'
+    'frontend',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
